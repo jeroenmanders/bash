388b5cbe46517b2549f711d937c57c6564d011fa
Jeroen Manders <jeroen@manders.be>
2022-02-06 12:55:27 +0100

    Add Helm install script

--- utils/env.sh
+++ utils/env.sh
@@ -3,7 +3,8 @@
 set -euo pipefail
 
 REPO_DIR="$(git rev-parse --show-toplevel)"
+BIN_DIR="$REPO_DIR/local-resources/bin"
 
 . "$REPO_DIR/bash/init.sh"
 
-[[ ! -d "$REPO_DIR/local-resources/bin" ]] && mkdir "$REPO_DIR/local-resources/bin"
+[[ ! -d "$BIN_DIR" ]] && mkdir "$BIN_DIR" || :
--- utils/install-helm.sh
+++ utils/install-helm.sh
@@ -5,24 +5,12 @@ cd "$this_dir" || exit 1
 
 . ./env.sh
 
-function ensure_packer() {
-  # PACKER_VERSION is set in bash/init.sh
-  local zip_file="packer_${PACKER_VERSION}_linux_amd64.zip"
-  local url="https://releases.hashicorp.com/packer/$PACKER_VERSION/$zip_file"
-
-  export PACKER="$REPO_DIR/local-resources/bin/packer-$PACKER_VERSION"
-  [[ -f "$PACKER" ]] && log_warn "$PACKER already exists. Using it instead of downloading a new binary." && return
-
-  log_info "Downloading $zip_file from $url."
-  curl -sLo "$zip_file" "$url"
-
-  log_info "Extracting archive."
-  unzip "$zip_file"
-  mv packer "$PACKER"
-  rm -Rf "$zip_file"
-
-  log_info "Using Packer:"
-  $PACKER --version
+function install_helm() {
+  log_info "Installing Helm"
+  curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+  chmod 700 get_helm.sh
+  ./get_helm.sh
+  rm get_helm.sh
 }
 
-ensure_packer
+install_helm
