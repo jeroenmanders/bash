30b9d8325e02de66b576fdc71d25b22a17089976
Jeroen Manders <jeroen@manders.be>
2022-02-20 10:44:23 +0100

    Add install-cmctl

---
+++ kubernetes/products/cert-manager/delete.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+this_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$this_dir" || exit 1
+
+. ./env.sh
+
+function delete_resources() {
+  log_info "Applying yaml files."
+  cat $(ls -r yaml/*.yaml) | envsubst | kubectl delete -n "$SERVICE_NAMESPACE" -f -
+}
+
+delete_resources
---
+++ utils/install-cmctl.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+. ./env.sh
+
+# cmctl is the cert-manager CLI
+
+CMCTL_VERSION="v1.7.1"
+
+function ensure_cmctl() {
+  log_info "Installing cmctl $CMCTL_VERSION."
+  OS=$(go env GOOS); ARCH=$(go env GOARCH); curl -sSL -o cmctl.tar.gz https://github.com/cert-manager/cert-manager/releases/download/v1.7.1/cmctl-$OS-$ARCH.tar.gz
+  tar xzf cmctl.tar.gz
+  sudo mv cmctl /usr/local/bin
+  rm cmctl.tar.gz
+}
+
+ensure_cmctl
